6804c7e26131479f0c11aa2fae5d48714f19c9ca
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-05-17 23:55:08 -0300

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { SkillSelection } from "./components/SkillSelection.jsx";
 import { SkillTest } from "./components/SkillTest.jsx";
+// import { User } from "./components/User.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
 	return (
-		<>
-			<SkillSelection />
-			<SkillTest />
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<SkillSelection />}></Route>
+				<Route path="/test/:testName" element={<SkillTest />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
