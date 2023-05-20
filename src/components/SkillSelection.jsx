5de635ce126ac455a7cd9659b394e6421f376eb8
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-05-20 15:44:44 -0300

    Add class to selection skills

--- src/components/SkillSelection.jsx
+++ src/components/SkillSelection.jsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+
+import "../style/skillSelection.css";
 export const SkillSelection = () => {
 	const navigate = useNavigate();
 
@@ -333,19 +335,27 @@ export const SkillSelection = () => {
 	return (
 		<>
 			<section className="selection">
-				<h2 className="selection__title"></h2>
+				<h2 className="selection__title">TEST DE APTITUDES</h2>
 
-				<section>
+				<section className="selection__questions">
 					{skills.map((el) => {
 						return (
-							<article key={el.testName}>
-								<h3>{el.testName}</h3>
-								<p>{el.description}</p>
-								<p className="grade">{el.grade}</p>
+							<article
+								className="selection__questions-content"
+								key={el.testName}>
+								<h3 className="selection__questions-title">{el.testName}</h3>
+								<p className="selection__questions-description">
+									{el.description}
+								</p>
+								<div className="selection__questions-interactive">
+									<p className="selection__questions-grade">{el.grade}</p>
 
-								<button onClick={() => handdleClick(el.testName)}>
-									View Questions
-								</button>
+									<button
+										className="selection__questions-button"
+										onClick={() => handdleClick(el.testName)}>
+										View Questions
+									</button>
+								</div>
 							</article>
 						);
 					})}
