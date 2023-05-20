fd72729d371a6ae84d4de98fead8addd79c5d10e
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-05-20 20:30:53 -0300

    Add Navbar elements with styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+import avatar from "../assets/avatar.png";
+import logo from "../assets/Logo.png";
+import "../style/navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav>
-			<ul>
-				<li>
-					<img src="" alt="" />
+		<nav className="navbar">
+			<ul className="navbar__list">
+				<li className="navbar__list-item">
+					<img src={logo} alt="" />
 				</li>
-				<li>Empleo</li>
-				<li>Mas Ofertas</li>
-				<li>CV</li>
-				<li>Quien me ve</li>
-				<li>Aptitudes</li>
+				<li className="navbar__list-unable">Empleo</li>
+				<li className="navbar__list-unable">Mas Ofertas</li>
+				<li className="navbar__list-unable">CV</li>
+				<li className="navbar__list-unable">Quien me ve</li>
+				<li className="navbar__list-item">Aptitudes</li>
 			</ul>
-			<div>
-				<img src="" alt="" />
+			<div className="navbar__user">
+				<img src={avatar} alt="" />
 				<img src="" alt="" />
 			</div>
 		</nav>
