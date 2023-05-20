965354090a4bd35d572631d89a6faf23f9bc9ee1
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-05-20 15:44:55 -0300

    Add component Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Navbar = () => {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<img src="" alt="" />
+				</li>
+				<li>Empleo</li>
+				<li>Mas Ofertas</li>
+				<li>CV</li>
+				<li>Quien me ve</li>
+				<li>Aptitudes</li>
+			</ul>
+			<div>
+				<img src="" alt="" />
+				<img src="" alt="" />
+			</div>
+		</nav>
+	);
+};
