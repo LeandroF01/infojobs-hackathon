6a59e39876339fb645ad28bc6dde6fd4e19209f8
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-05-22 19:15:29 -0300

    Add component cv

---
+++ src/components/Cv.jsx
@@ -0,0 +1,19 @@
+export const Cv = () => {
+	return (
+		<header className="profile">
+			<img className="profile__user" src="" alt="" />
+			<main className="profile__main">
+				<section className="main__user"></section>
+				<section className="main__add"></section>
+				<section className="main__experience"></section>
+				<section className="main__education"></section>
+				<section className="main__skills"></section>
+				<section className="main__other-data"></section>
+				<section className="main__status-preferences"></section>
+				<section className="main__other-info"></section>
+			</main>
+			<aside className="profile__aside"></aside>
+			<footer className="profile__footer"></footer>
+		</header>
+	);
+};
--- src/components/User.jsx
+++
@@ -1,3 +0,0 @@
-export const User = () => {
-	return <div>User</div>;
-};
