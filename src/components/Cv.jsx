f6d4d8376487a571e7e0404a89dccc18647a52bc
Leandro Farias <104572164+LeandroF01@users.noreply.github.com>
2023-05-26 15:18:42 -0300

    Add logic media screen

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -12,21 +12,64 @@ import Info from "../assets/cv/Frame 218.png";
 import Footer from "../assets/cv/Footer.png";
 import Sidebar from "../assets/cv/Sidebar.png";
 import star from "../assets/Group.png";
+
+import HeadingCell from "../assets/responsive/Heading.png";
+import PersonalDataCell from "../assets/responsive/CV-PersonalData-CA-Mvp1.png";
+import SuggestionCell from "../assets/responsive/Card-Suggestion.png";
+import ExperienceCell from "../assets/responsive/Frame 210.png";
+import StudiesCell from "../assets/responsive/Frame 211.png";
+import LanguageCell from "../assets/responsive/Frame 214.png";
+import SkillsCell from "../assets/responsive/Frame 215.png";
+import OtherCell from "../assets/responsive/Frame 216.png";
+import StatusCell from "../assets/responsive/Frame 217.png";
+import InfoCell from "../assets/responsive/Frame 218.png";
+import FooterCell from "../assets/responsive/Footer.png";
+import SidebarCell from "../assets/responsive/Sidebar.png";
+import { useState, useEffect } from "react";
+
 import "../style/cv.css";
 
 export const Cv = ({ skills }) => {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth < 1110);
+		};
+
+		// Detectar el ancho de la pantalla al cargar y al cambiar el tamaño de la ventana
+		window.addEventListener("resize", handleResize);
+		handleResize();
+
+		// Eliminar el event listener al desmontar el componente
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
 	const filter = skills.filter((skill) =>
 		skill.grade === "Aprobado" ? skill : null
 	);
-	console.log(filter);
 
 	return (
 		<header className="header">
-			<img className="header__user" src={Heading} alt="" />
 			<section className="profile">
 				<main className="profile__main">
-					<img className="main__user" src={PersonalData} alt="" />
-					<img className="main__add" src={Suggestion} alt="" />
+					<img
+						className="header__user"
+						src={isMobile ? HeadingCell : Heading}
+						alt=""
+					/>
+					<img
+						className="main__user"
+						src={isMobile ? PersonalDataCell : PersonalData}
+						alt=""
+					/>
+					<img
+						className="main__add"
+						src={isMobile ? SuggestionCell : Suggestion}
+						alt=""
+					/>
 					<section className="main__aptitudes">
 						<section className="aptitudes__header">
 							<h4 className="aptitudes__header-title">Aptitudes</h4>
@@ -53,20 +96,52 @@ export const Cv = ({ skills }) => {
 							)}
 						</ul>
 					</section>
-					<img className="main__experience" src={Experience} alt="" />
-					<img className="main__education" src={Studies} alt="" />
-					<img className="main__language" src={Language} alt="" />
-					<img className="main__skills" src={Skills} alt="" />
-					<img className="main__other-data" src={Other} alt="" />
-					<img className="main__status-preferences" src={Status} alt="" />
-					<img className="main__other-info" src={Info} alt="" />
+					<img
+						className="main__experience"
+						src={isMobile ? ExperienceCell : Experience}
+						alt=""
+					/>
+					<img
+						className="main__education"
+						src={isMobile ? StudiesCell : Studies}
+						alt=""
+					/>
+					<img
+						className="main__language"
+						src={isMobile ? LanguageCell : Language}
+						alt=""
+					/>
+					<img
+						className="main__skills"
+						src={isMobile ? SkillsCell : Skills}
+						alt=""
+					/>
+					<img
+						className="main__other-data"
+						src={isMobile ? OtherCell : Other}
+						alt=""
+					/>
+					<img
+						className="main__status-preferences"
+						src={isMobile ? StatusCell : Status}
+						alt=""
+					/>
+					<img
+						className="main__other-info"
+						src={isMobile ? InfoCell : Info}
+						alt=""
+					/>
 				</main>
 				<aside className="profile__aside">
-					<img className="aside" src={Sidebar} alt="" />
+					<img
+						className="aside"
+						src={isMobile ? SidebarCell : Sidebar}
+						alt=""
+					/>
 				</aside>
 			</section>
 			<footer className="profile__footer">
-				<img className="footer" src={Footer} alt="" />
+				<img className="footer" src={isMobile ? FooterCell : Footer} alt="" />
 			</footer>
 		</header>
 	);
